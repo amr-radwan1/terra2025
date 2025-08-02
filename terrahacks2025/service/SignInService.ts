8c11d1983ccaae37eb240648db4af30b019e4a96
Syed Ali Raza Rizvi <ali.riz6301@gmail.com>
2025-08-02 02:08:45 -0400

    merge functions to SignInService.ts

--- terrahacks2025/service/SignInService.ts
+++ terrahacks2025/service/SignInService.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/supabase/client"
 
 export const SignInService = {
-    async userSignUp( email_entered: string, password_entered: string){
+    async userSignIn(email_entered: string, password_entered: string){
         try{
             const { data, error } = await supabase.auth.signInWithPassword({
                 email: email_entered,
@@ -13,8 +13,26 @@ export const SignInService = {
                 throw error;
             }
             console.log("Logged in: ", data)
-        } catch(error){
+        } catch(error) {
             console.error("Error occured signing in")
+            throw error;
+        }
+    },
+    async userSignUp(email_entered: string, password_entered:string){
+
+        try{
+            const { data, error } = await supabase.auth.signUp({
+                email: email_entered,
+                password: password_entered
+            })
+
+            if (error){
+                console.error("Error signing up")
+                throw error;
+            }
+            console.log("User created!: ", data)
+        } catch(error){
+            console.error("Error occured creating an auth user")
             throw error;
         }
     }
